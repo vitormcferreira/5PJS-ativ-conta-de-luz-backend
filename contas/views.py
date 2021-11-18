5852e829e28831615b1e6e1950860c9067b49d39
Vitor <vitorpgmdr@gmail.com>
2021-11-18 18:26:07 -0300

    adicionar paginação

--- contas/views.py
+++ contas/views.py
@@ -1,15 +1,29 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 
 from contas.models import Conta
 from contas.serializers import ContaSerializer
 from django.db.models import Min, Max
 
 
+class ContaPagination(PageNumberPagination):
+    page_size = 12
+
+    def get_paginated_response(self, data):
+        return Response({
+            'num_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'count': self.page.paginator.count,
+            'results': data
+        })
+
+
 class ContaViewSet(viewsets.ModelViewSet):
     queryset = Conta.objects.all()
     serializer_class = ContaSerializer
+    pagination_class = ContaPagination
 
 
 @api_view(http_method_names=['GET'])
